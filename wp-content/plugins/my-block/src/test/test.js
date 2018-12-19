dd04393aee8cb9d2337be7d237394cd5122ae79a
Julie Frederick <juliefrederick@Julies-MacBook-Pro.local>
2018-12-19 11:00:09 -0500

    create guten blocks test

---
+++ wp-content/plugins/my-block/src/test/test.js
@@ -0,0 +1,73 @@
+/**
+ * BLOCK: test-block
+ *
+ * Registering a basic block with Gutenberg.
+ * Simple block, renders and saves the same content without any interactivity.
+ */
+
+//  Import CSS.
+import './test-style.css';
+import './editor.css';
+
+const { __ } = wp.i18n; // Import __() from wp.i18n
+const { registerBlockType } = wp.blocks; // Import registerBlockType() from wp.blocks
+const classes = {
+	headline: "test-block__headline"
+}
+/**
+ * Register: aa Gutenberg Block.
+ *
+ * Registers a new block provided a unique name and an object defining its
+ * behavior. Once registered, the block is made editor as an option to any
+ * editor interface where blocks are implemented.
+ *
+ * @link https://wordpress.org/gutenberg/handbook/block-api/
+ * @param  {string}   name     Block name.
+ * @param  {Object}   settings Block settings.
+ * @return {?WPBlock}          The block, if it has been successfully
+ *                             registered; otherwise `undefined`.
+ */
+registerBlockType( 'cgb/block-test-block', {
+	// Block name. Block names must be string that contains a namespace prefix. Example: my-plugin/my-custom-block.
+	title: __( 'test-block - CGB Block' ), // Block title.
+	icon: 'smiley', // Block icon from Dashicons → https://developer.wordpress.org/resource/dashicons/.
+	category: 'common', // Block category — Group blocks together based on common traits E.g. common, formatting, layout widgets, embed.
+	keywords: [
+		__( 'test-block — CGB Block' ),
+		__( 'my first block Example' ),
+		__( 'create-guten-block' ),
+	],
+
+	/**
+	 * The edit function describes the structure of your block in the context of the editor.
+	 * This represents what the editor will render when the block is used.
+	 *
+	 * The "edit" property must be a valid function.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+	 */
+	edit: function( props ) {
+		// Creates a <p class='wp-block-cgb-block-my-block'></p>.
+		return (
+			<div className={ props.className }>
+				<h1 class="testing-this-class">My first testing block!!!</h1>
+			</div>
+		);
+	},
+
+	/**
+	 * The save function defines the way in which the different attributes should be combined
+	 * into the final markup, which is then serialized by Gutenberg into post_content.
+	 *
+	 * The "save" property must be specified and must be a valid function.
+	 *
+	 * @link https://wordpress.org/gutenberg/handbook/block-api/block-edit-save/
+	 */
+	save: function( props ) {
+		return (
+			<div>
+				<h1 class="testing-this-class">My first testing block!!!</h1>
+			</div>
+		);
+	},
+} );
